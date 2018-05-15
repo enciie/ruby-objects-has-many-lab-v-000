6ecf724a412476bc96795362671d008d72a633b1
Nancy Jo <enciie@gmail.com>
2018-05-15 20:47:38 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -14,5 +14,11 @@ class Author
     @posts << post
     post.author = self
   end
-  
+
+  def add_post_by_title(post_title)
+    post = Post.new(post_title)
+    @posts << post
+    post.author = self
+  end
+
 end
