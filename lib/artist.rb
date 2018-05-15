5b8daa84c541c5eaab57ad89311d0ffe6d536c05
Nancy Jo <enciie@gmail.com>
2018-05-15 20:34:11 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 class Artist
   attr_accessor :name
-  @@song_count = 0 
+  @@song_count = 0
 
   def initialize(name)
     @name = name
@@ -24,7 +24,7 @@ class Artist
   end
 
   def self.song_count
-    @@song_count.length
+    @@song_count.length.to_i
   end
 
 end
